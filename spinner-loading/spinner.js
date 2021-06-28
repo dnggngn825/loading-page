16ac63196cadf571d39f288b8d71dcbb0befe92a
dnggngn825 <53506074+dnggngn825@users.noreply.github.com>
2021-06-28 15:27:46 +1000

    first commit

---
+++ spinner-loading/spinner.js
@@ -0,0 +1,8 @@
+{
+    let loader = document.querySelector(".loader-wrapper");
+            
+    window.addEventListener('load', function () {
+        // loader.classList.toggle('fade');
+        loader.style.display = 'none';
+    });
+}
